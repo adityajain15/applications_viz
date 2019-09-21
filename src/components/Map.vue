79ad3f1fb43a0e5e50b73da7ea4d42a66ee47d6d
Sue Roh <sueroh@gmail.com>
2019-09-21 19:44:45 -0400

    Changed map colors to dark theme

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -20,15 +20,19 @@ export default {
 
         this.map = new mapboxgl.Map({
           container: 'map', // container id
-          style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+          style: 'mapbox://styles/mapbox/dark-v9', // stylesheet location
           center: [-74.50, 40], // starting position [lng, lat]
-          zoom: 0 // starting zoom
+          zoom: 1 // starting zoom
         })    
         this.getResponses()
     this.interval = setInterval(this.getResponses, 3000)
 
   },
   methods: {
+    getRandomColor() {
+        let randomColor = "hsl(" + 360 * Math.random() + ',' + (25 + 70 * Math.random()) + '%,' + (85 + 10 * Math.random()) + '%)'
+        return randomColor
+    },
     getResponses() {
       fetch("https://laser-leotard.glitch.me/questions/0")
       .then(d=>d.json())
@@ -37,7 +41,7 @@ export default {
           if(!this.ids.includes(d[i].id)){
             var el = document.createElement('div')
             el.className = 'marker'
-            el.style.backgroundColor = 'purple'
+            el.style.backgroundColor = this.getRandomColor()
             el.style.width = '5px'
             el.style.height = '5px'
             el.style.borderRadius = '50%'
